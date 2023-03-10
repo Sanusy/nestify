3fc43d234f72a7c73a364eec53fd8fdfa430dc99
Ivan Moroziuk <morozyk.ivan@gmail.com>
2023-03-10 22:15:28 +0200

    NTF-3 > Added Localization

--- lib/core/nestify_app.dart
+++ lib/core/nestify_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:nestify/core/styles/app_theme.dart';
 import 'package:nestify/navigation/implementation/routes.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class NestifyApp extends StatelessWidget {
   const NestifyApp({Key? key}) : super(key: key);
@@ -10,6 +12,8 @@ class NestifyApp extends StatelessWidget {
     return MaterialApp.router(
       theme: appTheme,
       routerConfig: goRouter,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales:AppLocalizations.supportedLocales,
     );
   }
 }
--- lib/ui/first_screen/first_screen.dart
+++ lib/ui/first_screen/first_screen.dart
@@ -6,12 +6,14 @@ import 'package:nestify/navigation/navigation_service.dart';
 import 'package:nestify/redux/app_state.dart';
 import 'package:nestify/redux/navigation/navigation_action.dart';
 import 'package:redux/redux.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class FirstScreen extends StatelessWidget {
   const FirstScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final localized = AppLocalizations.of(context)!;
     return StoreConnector(
       builder: (BuildContext context, vm) {
         return Scaffold(
@@ -22,6 +24,7 @@ class FirstScreen extends StatelessWidget {
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Text(localized.testString),
               const Text('First screen'),
               const SizedBox(
                 height: 8,
