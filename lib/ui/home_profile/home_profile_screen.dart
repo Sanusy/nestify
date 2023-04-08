af4f917039ea4b3ae38fb40da3fdeb68230ab705
Ivan Moroziuk <morozyk.ivan@gmail.com>
2023-04-08 14:40:21 +0300

    NTF-13 > Added stub bottom navigation screens

---
+++ lib/ui/home_profile/home_profile_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:nestify/navigation/app_route.dart';
+import 'package:nestify/navigation/navigation_service.dart';
+import 'package:nestify/service/user_service/user_service.dart';
+
+class HomeProfileScreen extends StatelessWidget {
+  const HomeProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Text('Home profile screen'),
+            OutlinedButton(
+              onPressed: () async {
+                final navigationService = GetIt.instance.get<UserService>();
+                await navigationService.logOut();
+                GetIt.instance
+                    .get<NavigationService>()
+                    .replace(const AppRoute.login());
+              },
+              child: Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
