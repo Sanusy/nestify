cb88daf91a1415ef23267f546df6e0f9d4afb0ac
Ivan Moroziuk <morozyk.ivan@gmail.com>
2023-04-27 20:31:46 +0300

    NTF-21 > Added QR code

--- lib/ui/add_member/add_member_screen.dart
+++ lib/ui/add_member/add_member_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:nestify/gen/assets.gen.dart';
 import 'package:nestify/ui/add_member/add_member_view_model.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class AddMemberScreen extends StatelessWidget {
   final AddMemberViewModel viewModel;
@@ -38,46 +40,40 @@ class AddMemberScreen extends StatelessWidget {
         loaded: (loadedViewModel) {
           final qrCodeSize = MediaQuery.of(context).size.width - (80 * 2);
 
-          return CustomScrollView(
-            slivers: [
-              SliverPadding(
-                padding: const EdgeInsets.all(16),
-                sliver: SliverFillRemaining(
-                  hasScrollBody: false,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      const SizedBox(height: 48),
-                      Text(
-                        localization.addMemberDisclaimer,
-                        style: theme.textTheme.titleLarge,
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 16),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 64),
-                        child: Container(
-                          width: qrCodeSize,
-                          height: qrCodeSize,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      Text(
-                        localization.addMemberDisclaimerSubtitle,
-                        style: theme.textTheme.bodyLarge,
-                      ),
-                      const SizedBox(height: 64),
-                      const Spacer(),
-                      OutlinedButton(
-                        onPressed: loadedViewModel.onShareUrl,
-                        child: Text(localization.addMemberShareInvite),
-                      ),
-                    ],
+          return Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 48),
+                Text(
+                  localization.addMemberDisclaimer,
+                  style: theme.textTheme.titleLarge,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 64),
+                  child: QrImage(
+                    data: loadedViewModel.inviteUrl,
+                    size: qrCodeSize,
+                    // TODO: Replace with app icon
+                    embeddedImage: Assets.images.qrCodePicture.provider(),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 16),
+                Text(
+                  localization.addMemberDisclaimerSubtitle,
+                  style: theme.textTheme.bodyLarge,
+                ),
+                const SizedBox(height: 64),
+                const Spacer(),
+                OutlinedButton(
+                  onPressed: loadedViewModel.onShareUrl,
+                  child: Text(localization.addMemberShareInvite),
+                ),
+              ],
+            ),
           );
         },
       ),
